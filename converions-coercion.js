0f06537a5be26e0432152dd15f721a29d3e528df
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-09 02:08:44 +0530

    type conversion and type coercion

---
+++ converions-coercion.js
@@ -0,0 +1,20 @@
+// type conversion and type coercion
+let num =8;
+console.log(num, typeof num)
+let num1=String(num)
+console.log(num1,typeof num1)
+let str ="123"
+let str1=Number(str)
+console.log(str1, typeof str1)
+
+let x 
+console.log(typeof x)
+x=9
+console.log(typeof x)
+x=x+"a"
+console.log(typeof x)
+
+// coercion means when we add two data types which one should be the resultant one
+
+x=x-2
+console.log(x,typeof x)
