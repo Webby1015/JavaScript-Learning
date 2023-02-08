e43f17145d51f2e6d9080c62e036dacddcff7175
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-09 01:52:09 +0530

    objects

---
+++ object.js
@@ -0,0 +1,14 @@
+let alien = {
+    name:"abhishek",
+    age:22,
+    technologies:['python','js','html'],
+    'sex':'male'
+}
+let input='name'
+console.log(alien)
+console.log(typeof(alien))
+console.log(alien.name)
+console.log(alien['sex'])
+console.log(alien['name'])
+console.log(alien[input])
+
